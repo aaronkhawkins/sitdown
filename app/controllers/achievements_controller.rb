2ef82b8ed126a84a59555101805aaca6c91b83cd
Tim Shadel <tim.shadel@shawkvalue.com>
2008-05-22 09:41:39 -0700

    Add effects for the achievement changes
    * Current user only
    * Cleaned up the formatting for lists a bit too
    
    Signed-off-by: Tim Shadel <tim.shadel@shawkvalue.com>

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -6,7 +6,8 @@ class AchievementsController < ApplicationController
     if @achievement.save!
       render :update do |page| 
         page["side-achievement-#{current_user.id}"].replace_html :partial => "users/side_achievements", :locals => { :user => current_user }
-        page.insert_html :top, "day-#{@achievement.day.to_i}", :partial => "users/day_achievement", :locals => { :ach => @achievement }
+        page.insert_html :top, "day-#{@achievement.day.to_i}", :partial => "users/day_achievement", :locals => { :ach => @achievement, :hide => true }
+        page.visual_effect :blind_down, "ach-#{@achievement.id}", :duration => 0.5
         page['new_achievement'].reset
         #page << "eventSource.clear()"
         #page << "Timeline.loadXML(\"#{url_for(@user)}.xml\", function(xml, url) { eventSource.loadXML(xml, url); });"
